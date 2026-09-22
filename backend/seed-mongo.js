eaf58158fcced6cd0ed83765c9dbb4c7c9b1b4a6
angadalayuvarani <angadalayuvarani@gmail.com>
2026-09-22 21:28:08 +0530

    Fix mongodb model path

--- backend/seed-mongo.js
+++ backend/seed-mongo.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const User = require("./models/User");
+const User = require("./models/user");
 
 async function seedAdmin() {
   try {
